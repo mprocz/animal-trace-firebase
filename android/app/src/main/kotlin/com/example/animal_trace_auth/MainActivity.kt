86426a75f74911220cdf4d0649cba551eecc9e74
mprocz <marcosprocykevicz@gmail.com>
2024-12-10 15:08:27 -0300

    primeiro

---
+++ android/app/src/main/kotlin/com/example/animal_trace_auth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animal_trace_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/register_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return LoginPage();
+          }
+        }),
+      )
+    );
+  }
+}
